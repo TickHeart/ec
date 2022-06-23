488573ac939c0b029d7ef6aa8e8d7d94635bf827
tickheart <66043405+TickHeart@users.noreply.github.com>
2022-06-23 16:43:37 +0800

    Upload changes

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
+import dayjs from 'dayjs'
 import inquirer from 'inquirer'
 import { easyCreate } from './easyCreate'
 import { makePackage } from './makePackage'
+import chalk from 'chalk'
 
 const questions = [
   {
@@ -14,10 +16,14 @@ const questions = [
 async function allocatingExecutionFlow() {
   const { type } = await inquirer.prompt(questions)
   if (type === 'EasyCreate') {
-    easyCreate()
+    await easyCreate()
   } else {
-    makePackage()
+    await makePackage()
   }
+  var now = dayjs().format('YYYY-MM-DD HH:mm:ss')
+  console.log(
+    chalk.blue(`Now ${now}. Time flies and the years fly by. Come on Junior !`)
+  )
 }
 
 allocatingExecutionFlow()
